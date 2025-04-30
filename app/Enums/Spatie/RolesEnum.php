7b7115f71fa27f29e91305e5a255d6a230d75770
atomjoy <atomjoy.official@gmail.com>
2025-04-30 11:55:34 +0200

    Create RolesEnum.php

---
+++ app/Enums/Spatie/RolesEnum.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Enums\Spatie;
+
+enum RolesEnum: string
+{
+    case ADMIN = 'admin';
+    case WRITER = 'writer';
+    case SUPERADMIN = 'super_admin';
+
+    public function label(): string
+    {
+        return match ($this) {
+            static::ADMIN => 'Admins',
+            static::WRITER => 'Writers',
+            static::SUPERADMIN => 'Super Admins',
+            default => throw new \Exception('Unknown enum value requested for the label.'),
+        };
+    }
+}
